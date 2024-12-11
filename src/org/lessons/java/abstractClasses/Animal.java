e8ec8c5355ba5ed01977f21d242877edc7c138fb
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-12-11 20:37:52 +0100

    added method Sleep and abstract methods to abstract class Animal

--- src/org/lessons/java/abstractClasses/Animal.java
+++ src/org/lessons/java/abstractClasses/Animal.java
@@ -2,4 +2,12 @@ package org.lessons.java.abstractClasses;
 
 public abstract class Animal {
 
+    void sleep(){
+        System.out.println("Zzzz");
+    }
+
+    abstract void sound();
+
+    abstract void eat();
+
 }
